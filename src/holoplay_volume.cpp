276ca7fcd6bda3e1b713e4d972952d8434df82ea
CaptainProton42 <john.wigg@gmx.net>
2021-10-16 17:17:52 +0200

    Open window on Looking Glass.

--- src/holoplay_volume.cpp
+++ src/holoplay_volume.cpp
@@ -5,11 +5,60 @@
 #include <godot_cpp/classes/global_constants.hpp>
 #include <godot_cpp/classes/label.hpp>
 #include <godot_cpp/variant/utility_functions.hpp>
+#include <godot_cpp/classes/display_server.hpp>
+
+#include "holoplay_manager.h"
 
 #include "HoloPlayCore.h"
 
 using namespace godot;
 
 void HoloPlayVolume::_bind_methods() {
-
+    ClassDB::bind_method(D_METHOD("get_device_index"), &HoloPlayVolume::get_device_index);
+    ClassDB::bind_method(D_METHOD("set_device_index", "device_index"), &HoloPlayVolume::set_device_index);
+    // Properties.
+    ADD_PROPERTY(PropertyInfo(Variant::INT, "device_index"), "set_device_index", "get_device_index");
 }
+
+bool HoloPlayVolume::_is_headless() const {
+    return headless;
+}
+
+void HoloPlayVolume::_set_headless(bool p_headless) {
+    if (p_headless) {
+        if (window_id) {
+            DisplayServer::get_singleton()->delete_sub_window(window_id);
+            window_id = 0;
+        }
+    } else {
+        if (!window_id) {
+            uint32_t flags = DisplayServer::WINDOW_FLAG_BORDERLESS | DisplayServer::WINDOW_FLAG_ALWAYS_ON_TOP | DisplayServer::WINDOW_FLAG_NO_FOCUS;
+            int win_x = hpc_GetDevicePropertyWinX(internal_device_index);
+            int win_y = hpc_GetDevicePropertyWinY(internal_device_index);
+            int width = hpc_GetDevicePropertyScreenW(internal_device_index);
+            int height = hpc_GetDevicePropertyScreenH(internal_device_index);
+            window_id = DisplayServer::get_singleton()->create_sub_window(DisplayServer::WINDOW_MODE_WINDOWED, DisplayServer::VSYNC_DISABLED, 0, Rect2(win_x, win_y, width, height));
+        }
+    }
+    headless = p_headless;
+}
+
+void HoloPlayVolume::set_device_index(int p_device_index) {
+    Array connected_devices = HoloPlayManager::get_singleton()->get_connected_devices();
+    if (p_device_index + 1 > connected_devices.size()) {
+        _set_headless(true);
+        managed_device_index = p_device_index;
+        return;
+    }
+
+    internal_device_index = connected_devices.slice(p_device_index, p_device_index).front(); // [] operator is not implemented yet.
+    _set_headless(false);
+    managed_device_index = p_device_index;
+}
+
+
+int HoloPlayVolume::get_device_index() const {
+    return managed_device_index;
+}
+
+HoloPlayVolume::HoloPlayVolume() { }
--- src/holoplay_volume.h
+++ src/holoplay_volume.h
@@ -4,6 +4,7 @@
 #include <godot_cpp/classes/node3d.hpp>
 #include <godot_cpp/classes/global_constants.hpp>
 #include <godot_cpp/classes/viewport.hpp>
+#include <godot_cpp/core/defs.hpp>
 
 #include <godot_cpp/core/binder_common.hpp>
 
@@ -12,13 +13,25 @@ using namespace godot;
 class HoloPlayVolume : public Node3D {
 	GDCLASS(HoloPlayVolume, Node3D);
 
+private:
+	int internal_device_index; // Device index used by the HoloPlayCore API.
+	int managed_device_index = 0; // Device index used by the HoloPlayManager.
+	bool headless = false;
+
+	int window_id = 0;
+
 protected:
 	static void _bind_methods();
 
 private:
+	void _set_headless(bool p_headless);
+	bool _is_headless() const;
 
 public:
+	void set_device_index(int p_device_index);
+	int get_device_index() const;
 
+	HoloPlayVolume();
 };
 
 #endif // ! HOLOPLAY_VOLUME_H
